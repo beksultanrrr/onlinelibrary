5f71b942bcb53c3155f8361379fb30e0dcb1b2d3
Yerkhat Sakhtashov <200103307@stu.sdu.edu.kz>
2023-10-19 04:11:49 +0600

    validator

--- internal/data/books.go
+++ internal/data/books.go
@@ -3,6 +3,7 @@ import (
 	 "time"
 	 "fmt"
 	 "encoding/json"
+	 "onlinelibrary.beks.net/internal/validator" 
 )
 
 
@@ -23,7 +24,24 @@ type Book struct {
 
 
 }
-
+func ValidateBook(v *validator.Validator, book *Book) {
+	v.Check(book.Title != "", "title", "must be provided")
+	v.Check(len(book.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(book.Year != 0, "year", "must be provided")
+	v.Check(book.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(book.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(book.Readtime != 0, "readtime", "must be provided")
+	v.Check(book.Readtime > 0, "readtime", "must be a positive integer")
+	v.Check(book.Genres != nil, "genres", "must be provided")
+	v.Check(len(book.Genres) >= 1, "genres", "must contain at least 1 genre") 
+	v.Check(validator.Unique(book.Genres), "genres", "must not contain duplicate values")
+	v.Check(book.Language != nil, "language", "must be provided")
+	v.Check(len(book.Language) >= 1, "language", "must contain at least 1 language") 
+	v.Check(book.Rating <=10 ,  "rating", "must be less than 10")
+	v.Check(book.Author != "", "author", "must be provided")
+	v.Check(len(book.Author) <= 500, "author", "must not be more than 500 bytes long")
+	v.Check(book.PageCount >0 ,  "pagecount", "must be less than 0")
+	}
 func (m Book) MarshalJSON() ([]byte, error) {
 	// Declare a variable to hold the custom readtime string (this will be the empty // string "" by default).
 	var readtime string
